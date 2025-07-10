8b3da73ff93255535ef587b8f97f10c53f3098a9
corlay <v.corlay@fr.merce.mee.com>
2025-07-10 12:04:36 +0200

    Add local and global models

--- Database_edition/AI_treatment/settings.py
+++ Database_edition/AI_treatment/settings.py
@@ -5,7 +5,7 @@ Configuration setting.
 import os
 
 # Together AI API settings
-TOGETHER_API_KEY = "ac424c0d51eb7ace7194828eb9d340e9e4964c85c27b92d52e70eaaac9ab5569" #add your own key
+TOGETHER_API_KEY = "" #add your own key
 
 # Equation check settings
 CHECK_EQUATIONS_FILE = None  # Set to file path to process single file, None to process all files in OUTPUT_DIR
